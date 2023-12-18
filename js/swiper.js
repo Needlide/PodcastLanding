e76065a04bed5531f0beb1b1647da638caad2deb
Needlide <max.litvinyuck553@gmail.com>
2023-12-18 19:24:22 +0000

    Extracted classes, placed SVG, added minor changes to the margins and paddings in styles

--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-      var gallerySwiper = new Swiper('.gallery', {
-        slidesPerView: 5,
-        spaceBetween: 25,
-        loop: true,
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  var gallerySwiper = new Swiper(".hero__gallery", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+  });
 
-      var feedbacksSwiper = new Swiper('.feedbacks-list', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        navigation: {
-          nextEl: '.navigation-right',
-          prevEl: '.navigation-left',
-        }
-      });
-    });
+  var feedbacksSwiper = new Swiper(".feedbacks-list", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".navigation-right",
+      prevEl: ".navigation-left",
+    },
+  });
+});
